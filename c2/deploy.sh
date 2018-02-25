e50c42173109bd36370e078a323b0f78fa8c2c6a
Sean O'Connor <sean@bit.ly>
2018-02-25 17:48:12 -0500

    Simplified deploy scripts.

--- c2/deploy.sh
+++ c2/deploy.sh
@@ -2,35 +2,7 @@
 
 set -e
 
-function usage()
-{
-    echo "Script used for pushing new code out."
-    echo ""
-    echo "./deploy.sh"
-    echo "\t-h --help"
-    echo "\t--version=$VERSION"
-    echo ""
-}
+TAG="v-`git rev-parse --short HEAD`"
 
-while [ "$1" != "" ]; do
-    PARAM=`echo $1 | awk -F= '{print $1}'`
-    VALUE=`echo $1 | awk -F= '{print $2}'`
-    case $PARAM in
-        -h | --help)
-            usage
-            exit
-            ;;
-        --version)
-            VERSION=$VALUE
-            ;;
-        *)
-            echo "ERROR: unknown parameter \"$PARAM\""
-            usage
-            exit 1
-            ;;
-    esac
-    shift
-done
-
-gcloud container builds submit --tag gcr.io/blinky-196302/c2-image:v$VERSION
-kubectl --cluster=gke_blinky-196302_us-east1-b_home set image deployment/c2-server c2-server=gcr.io/blinky-196302/c2-image:v$VERSION
+gcloud container builds submit --tag gcr.io/blinky-196302/c2-image:$TAG
+kubectl --cluster=gke_blinky-196302_us-east1-b_home set image deployment/c2-server c2-server=gcr.io/blinky-196302/c2-image:$TAG
--- pops/deploy.sh
+++ pops/deploy.sh
@@ -6,42 +6,14 @@ POPS=(
 
 set -e
 
-function usage()
-{
-    echo "Script used for pushing new code out to all POPs."
-    echo ""
-    echo "./deploy.sh"
-    echo "\t-h --help"
-    echo "\t--version=$VERSION"
-    echo ""
-}
+TAG="v-`git rev-parse --short HEAD`"
 
-while [ "$1" != "" ]; do
-    PARAM=`echo $1 | awk -F= '{print $1}'`
-    VALUE=`echo $1 | awk -F= '{print $2}'`
-    case $PARAM in
-        -h | --help)
-            usage
-            exit
-            ;;
-        --version)
-            VERSION=$VALUE
-            ;;
-        *)
-            echo "ERROR: unknown parameter \"$PARAM\""
-            usage
-            exit 1
-            ;;
-    esac
-    shift
-done
-
-gcloud container builds submit --tag gcr.io/blinky-196302/pop-image:v$version
+gcloud container builds submit --tag gcr.io/blinky-196302/pop-image:$TAG
 for POP in ${POPS[@]}; do
     echo "Updating $POP to v$VERSION"
     CLUSTER_NAME="pop-$POP"
     K8S_CLUSTER_NAME="gke_blinky-196302_"$POP"_$CLUSTER_NAME"
 
-    kubectl  --cluster=$K8S_CLUSTER_NAME set image deployment/pop-process pop-process=gcr.io/blinky-196302/pop-image:v$VERSION
+    kubectl  --cluster=$K8S_CLUSTER_NAME set image deployment/pop-process pop-process=gcr.io/blinky-196302/pop-image:$TAG
 
 done
