14c1e97b0be54ebff5cbe9d81e04b35ce1cc6865
Sean O'Connor <sean@bit.ly>
2018-02-25 15:42:39 -0500

    Got the django admin working.

--- c2/pops/admin.py
+++ c2/pops/admin.py
@@ -3,4 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from .models import POP, Process
+
+
+admin.site.register(POP)
+admin.site.register(Process)
